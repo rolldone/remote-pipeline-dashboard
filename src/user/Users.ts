890041c4d8e802974870c20cab2c241011f34da3
donny rolanda <donny.rolanda@gmail.com>
2022-03-30 10:32:17 +0800

    Create the page design for all route - Create html on user page

--- src/user/Users.ts
+++ src/user/Users.ts
@@ -1,6 +1,29 @@
 import BaseRactive from "base/BaseRactive";
+import { BrowserHistoryEngine, createRouter } from "routerjs";
 import template from './UsersView.html';
 
 export default BaseRactive.extend({
-  template
+  template,
+  onconfig() {
+    this.router = createRouter({
+      engine: BrowserHistoryEngine({ bindClick: false }),
+      basePath: "/dashboard/user"
+    })
+      // Define the route matching a path with a callback
+      .get('/new', async (req, context) => {
+        // Handle the route here...
+        let User = (await import("./UserNew")).default;
+        new User({
+          target: "#index-body",
+        })
+      })
+      .get('/:id/view', async (req, context) => {
+        // Handle the route here...
+        let User = (await import("./UserUpdate")).default;
+        new User({
+          target: "#index-body",
+        })
+      })
+      .run();
+  }
 });
