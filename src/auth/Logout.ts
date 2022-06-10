a7e89f48415b3928aa9798fd5bc7b8fb19e5928b
donny <donny.rolanda@gmail.com>
2022-06-10 09:04:56 +0800

    Improvement
    - [Logout] : Add logout page

--- src/auth/Logout.ts
+++ src/auth/Logout.ts
@@ -1,6 +1,27 @@
-import BaseRactive from "base/BaseRactive";
+import BaseRactive, { BaseRactiveInterface } from "base/BaseRactive";
+import AuthService from "services/AuthService";
 import template from './LogoutView.html';
 
-export default BaseRactive.extend({
-  template
+export interface LogoutInterface extends BaseRactiveInterface {
+  submitLogout: { (): void }
+}
+
+export default BaseRactive.extend<LogoutInterface>({
+  template,
+  oncomplete() {
+    let _super = this._super.bind(this);
+    return new Promise((resolve: Function) => {
+      this.submitLogout();
+      _super();
+      resolve();
+    })
+  },
+  async submitLogout() {
+    try {
+      let resData = await AuthService.logoutService({});
+      return resData;
+    } catch (ex) {
+      console.error("submitLogout - ex :: ", ex);
+    }
+  }
 });
