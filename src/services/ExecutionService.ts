f39db1c6d983c3bcc21b84fee5519ff797d1c625
donny <donny.rolanda@gmail.com>
2022-09-29 09:47:02 +0800

    Ondevelop
    - [AppModular] : Convert big project to be modular

--- src/services/ExecutionService.ts
+++ src/services/ExecutionService.ts
@@ -1,6 +1,5 @@
 import SqlBricks from "./SqlBricks";
 import SqlService from "./core/SqlService";
-import QueueService from "./core/QueueService";
 import BaseService from "./BaseService";
 import axios from "axios";
 import SmartUrlSearchParams from "base/SmartUrlSearchParams";
@@ -74,30 +73,6 @@ export default {
     } catch (ex) {
       throw ex;
     }
-    // try {
-    //   let resData = await SqlService.insert(SqlBricks.insert('executions', {
-    //     id: props.id,
-    //     name: props.name,
-    //     process_mode: props.process_mode,
-    //     process_limit: props.process_limit,
-    //     pipeline_id: props.pipeline_id,
-    //     project_id: props.project_id,
-    //     user_id: props.user_id,
-    //     variable_id: props.variable_id,
-    //     variable_option: props.variable_option,
-    //     pipeline_item_ids: JSON.stringify(props.pipeline_item_ids),
-    //     host_ids: JSON.stringify(props.host_ids),
-    //     description: props.description,
-    //   }).toString());
-    //   resData = await SqlService.selectOne(SqlBricks.select("*").from("executions").where("id", resData).toString());
-    //   return {
-    //     status: 'success',
-    //     status_code: 200,
-    //     return: resData
-    //   }
-    // } catch (ex) {
-
-    // }
   },
   async updateExecution(props: ExecutionServiceInterface) {
     try {
@@ -128,29 +103,6 @@ export default {
     } catch (ex) {
       throw ex;
     }
-    // try {
-    //   let resData = await SqlService.update(SqlBricks.update('executions', {
-    //     name: props.name,
-    //     process_mode: props.process_mode,
-    //     process_limit: props.process_limit,
-    //     pipeline_id: props.pipeline_id,
-    //     project_id: props.project_id,
-    //     user_id: props.user_id,
-    //     variable_id: props.variable_id,
-    //     variable_option: props.variable_option,
-    //     pipeline_item_ids: JSON.stringify(props.pipeline_item_ids),
-    //     host_ids: JSON.stringify(props.host_ids),
-    //     description: props.description,
-    //   }).where("id", props.id).toString());
-    //   resData = await SqlService.selectOne(SqlBricks.select("*").from("executions").where("id", props.id).toString());
-    //   return {
-    //     status: 'success',
-    //     status_code: 200,
-    //     return: resData
-    //   }
-    // } catch (ex) {
-
-    // }
   },
   deleteExecution: async function (props: ExecutionServiceInterface) {
     try {
@@ -178,57 +130,6 @@ export default {
     } catch (ex) {
       throw ex;
     }
-    // try {
-    //   SqlBricks.aliasExpansions({
-    //     'pro': "projects",
-    //     "pip": "pipelines",
-    //     "pip_item": "pipeline_items",
-    //     "var": "variables",
-    //     'usr': "users",
-    //     'exe': "executions",
-    //   });
-    //   let query = SqlBricks.select(
-    //     'exe.id as id',
-    //     'exe.name as name',
-    //     'exe.process_mode as process_mode',
-    //     'exe.process_limit as process_limit',
-    //     'exe.pipeline_id as pipeline_id',
-    //     'exe.project_id as project_id',
-    //     'exe.user_id as user_id',
-    //     'exe.variable_id as variable_id',
-    //     'exe.variable_option as variable_option',
-    //     'exe.pipeline_item_ids as pipeline_item_ids',
-    //     'exe.host_ids as host_ids',
-    //     'exe.description as description',
-    //     'pro.name as pro_name',
-    //     'pip.name as pip_name',
-    //     'var.name as var_name'
-    //   ).from("exe");
-    //   query = query.leftJoin('pro').on({
-    //     "pro.id": "exe.project_id"
-    //   });
-    //   query = query.leftJoin('pip').on({
-    //     "pip.id": "exe.pipeline_id"
-    //   });
-    //   query = query.leftJoin("usr").on({
-    //     "usr.id": "exe.user_id"
-    //   });
-    //   query = query.leftJoin("var").on({
-    //     "var.id": "exe.variable_id"
-    //   });
-    //   if (props.user_id != null) {
-    //     query = query.where("usr.id", props.user_id);
-    //   }
-    //   query = query.orderBy("exe.id DESC");
-    //   let resData = await SqlService.select(query.toString());
-    //   return {
-    //     status: 'success',
-    //     status_code: 200,
-    //     return: resData
-    //   }
-    // } catch (ex) {
-    //   throw ex;
-    // }
   },
   async getExecution(props: any) {
     try {
@@ -240,59 +141,6 @@ export default {
     } catch (ex) {
       throw ex;
     }
-    // try {
-    //   SqlBricks.aliasExpansions({
-    //     'pro': "projects",
-    //     "pip": "pipelines",
-    //     "pip_item": "pipeline_items",
-    //     "var": "variables",
-    //     'usr': "users",
-    //     'exe': "executions",
-    //   });
-    //   let query = SqlBricks.select(
-    //     'exe.id as id',
-    //     'exe.name as name',
-    //     'exe.process_mode as process_mode',
-    //     'exe.process_limit as process_limit',
-    //     'exe.pipeline_id as pipeline_id',
-    //     'exe.project_id as project_id',
-    //     'exe.user_id as user_id',
-    //     'exe.variable_id as variable_id',
-    //     'exe.variable_option as variable_option',
-    //     'exe.pipeline_item_ids as pipeline_item_ids',
-    //     'exe.host_ids as host_ids',
-    //     'exe.description as description',
-    //     'pro.name as pro_name',
-    //     'pip.name as pip_name',
-    //     'var.name as var_name'
-    //   ).from("exe");
-    //   query = query.leftJoin('pro').on({
-    //     "pro.id": "exe.project_id"
-    //   });
-    //   query = query.leftJoin('pip').on({
-    //     "pip.id": "exe.pipeline_id"
-    //   });
-    //   query = query.leftJoin("usr").on({
-    //     "usr.id": "exe.user_id"
-    //   });
-    //   query = query.leftJoin("var").on({
-    //     "var.id": "exe.variable_id"
-    //   });
-    //   if (props.user_id != null) {
-    //     query = query.where("usr.id", props.user_id);
-    //   }
-    //   query = query.where("exe.id", props.id);
-    //   query = query.orderBy("exe.id DESC");
-    //   query = query.limit(1);
-    //   let resData = await SqlService.selectOne(query.toString());
-    //   return {
-    //     status: 'success',
-    //     status_code: 200,
-    //     return: resData
-    //   }
-    // } catch (ex) {
-    //   throw ex;
-    // }
   },
   async runExecution(props) {
     try {
@@ -310,64 +158,5 @@ export default {
     } catch (ex) {
       throw ex;
     }
-    // try {
-    //   /* Get the execution data first */
-    //   SqlBricks.aliasExpansions({
-    //     'pro': "projects",
-    //     "pip": "pipelines",
-    //     "pip_item": "pipeline_items",
-    //     "var": "variables",
-    //     'usr': "users",
-    //     'exe': "executions",
-    //   });
-    //   let query = SqlBricks.select(
-    //     'exe.id as id',
-    //     'exe.name as name',
-    //     'exe.process_mode as process_mode',
-    //     'exe.process_limit as process_limit',
-    //     'exe.pipeline_id as pipeline_id',
-    //     'exe.project_id as project_id',
-    //     'exe.user_id as user_id',
-    //     'exe.variable_id as variable_id',
-    //     'exe.variable_option as variable_option',
-    //     'exe.pipeline_item_ids as pipeline_item_ids',
-    //     'exe.host_ids as host_ids',
-    //     'exe.description as description',
-    //     'pro.name as pro_name',
-    //     'pip.name as pip_name',
-    //     'var.name as var_name'
-    //   ).from("exe");
-    //   query = query.leftJoin('pro').on({
-    //     "pro.id": "exe.project_id"
-    //   });
-    //   query = query.leftJoin('pip').on({
-    //     "pip.id": "exe.pipeline_id"
-    //   });
-    //   query = query.leftJoin("usr").on({
-    //     "usr.id": "exe.user_id"
-    //   });
-    //   query = query.leftJoin("var").on({
-    //     "var.id": "exe.variable_id"
-    //   });
-    //   if (props.user_id != null) {
-    //     query = query.where("usr.id", props.user_id);
-    //   }
-    //   query = query.where("exe.id", props.id);
-    //   query = query.orderBy("exe.id DESC");
-    //   query = query.limit(1);
-    //   let resData = await SqlService.selectOne(query.toString());
-    //   /* Generate the on bull queue */
-    //   let resDataQueue = await QueueService.queue(resData.id);
-    //   if (resDataQueue == "") {
-    //     let _form_data = new FormData();
-    //     _form_data.append("id", resData.id);
-    //     _form_data.append("data", "{}");
-    //     _form_data.append("process_mode_path", resData.process_mode == "sequential" ? "queue.request.basic" : "queue.request.parallel");
-    //     resDataQueue = await QueueService.create(_form_data);
-    //     debugger;
-    //   }
-    // } catch (ex) {
-    //   throw ex;
-    // }
   }
 }
